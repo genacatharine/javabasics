86bd8363c1c9e5bbaf2f2769a2b7a286c58de5ec
Gena Catherine <gena.catharine@gmail.com>
2018-02-03 11:31:23 -0700

    different variable types

--- Application.java
+++ Application.java
@@ -3,6 +3,19 @@ public class Application {
   public static void main(String [] args){
     int myNumber;
     myNumber = 88;
+
+    double myDouble=3.1452;
+
+    char myChar = 'y';
+    boolean myBoolean = true;
+
+    byte myByte = 127;
+
+
     System.out.println(myNumber);
+    System.out.println(myDouble);
+    System.out.println(myChar);
+    System.out.println(myBoolean);
+    System.out.println(myByte);
   }
 }
