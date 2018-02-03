6c25d43826652e27935a721ee8f21d03d0cab7c1
Gena Catherine <gena.catharine@gmail.com>
2018-02-03 13:08:54 -0700

    for loop

--- Application.java
+++ Application.java
@@ -32,14 +32,25 @@
 //     System.out.println(greeting); //semicolons are important!
 //   }
 // }
+
+//while loop
+// public class Application {
+//   public static void main(String[] args) {
+//     int value = 0;
+//
+//     while(value < 10){
+//           System.out.println("Hello"+ value);
+//           value = value + 1;
+//     }
+//
+//   }
+// }
+//for loop
 public class Application {
-  public static void main(String[] args) {
-    int value = 0;
+    public static void main(String[] args) {
 
-    while(value < 10){
-          System.out.println("Hello"+ value);
-          value = value + 1;
+      for(int i= 0;i < 5; i++){
+        System.out.println("Hello");
+      }
     }
-
-  }
 }
